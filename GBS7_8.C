833e9bc7ec71163a85d61fae4730feedd4c582e8
kps004 <36299014+kps004@users.noreply.github.com>
2019-02-04 18:52:30 +0530

    Create GBS7_8.C

---
+++ GBS7_8.C
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   int n;
+   scanf("%d",&n);
+   if(n%7==0)
+   {
+       printf("yes");
+   }
+   else
+   {
+       printf("No");
+   }
+
+    return 0;
+}
