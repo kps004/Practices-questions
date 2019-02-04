9aa2a32e1f2c0585ce4136cef69a82ebcd8f6bdf
kps004 <36299014+kps004@users.noreply.github.com>
2019-02-04 18:34:45 +0530

    Create GBS7_5.C

---
+++ GBS7_5.C
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char n[500];
+    int i,j,k;
+    scanf("%[^\n]",n);
+  // printf("\n");
+  for(i=0;n[i]!='\0';i++)
+  {
+      if(n[i]!=';')
+      {
+          printf("%c",n[i]);
+      }
+  }
+   
+    
+
+    return 0;
+}
