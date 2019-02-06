0e396febfb9c4eebbbc5333a9eab83239c31a646
kps004 <36299014+kps004@users.noreply.github.com>
2019-02-06 21:24:32 +0530

    Create GBS9_2.C

---
+++ GBS9_2.C
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    
+    float l,b;
+    scanf("%f %f",&l,&b);
+    l=l*b;
+    printf("%.5f",l);
+    
+
+    return 0;
+}
