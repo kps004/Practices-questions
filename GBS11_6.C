f55e1661323d236e11eb66e447dd25b4d912264e
kps004 <36299014+kps004@users.noreply.github.com>
2019-02-17 22:32:08 +0530

    Update GBS11_6.C

--- GBS11_6.C
+++ GBS11_6.C
@@ -1,26 +1,38 @@
+/******************************************************************************
 
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
 
 #include <stdio.h>
 
 int main()
 {
-    int a[20],i,n,k,j;
+    int a[20],i,n,k,j,p;
     scanf("%d%d",&n,&k);
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
-   k--;
+  
     
         for(j=0,i=0;(j<n)&&(i<=k);j++)
         {
             if(a[j]%2==1)
             {
+                 p=j;
                 i++;
+                if(a[j]==a[j+1])
+                {
+                  j++;
+                }
+            
             }
         }
-        j--;
-        printf(" %d",a[j]);
+      
+        printf(" %d",a[p]);
 
     return 0;
 }
