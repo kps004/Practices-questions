4d2f7aa762c200b4583a8d27cd91c4e4a5e9086d
kps004 <36299014+kps004@users.noreply.github.com>
2019-02-19 20:22:39 +0530

    Create GBS11_8.C

---
+++ GBS11_8.C
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main ()
+{
+
+  int a[10], i, j, n, p=0, q=0, k=0;
+  scanf("%d  %d", &n, &k);
+  for (i = 0; i < n; i++)
+    {
+      scanf("%d", &a[i]);
+    }
+  p = a[0];
+  for (i = 0; i < k; i++)
+    {
+      for (j = 1; j < n; j++)
+	{
+	  if ((p > a[j])&&(a[j]!=32436))
+	    {
+	      p = a[j];
+	      q = j;
+	    }
+	}
+      a[q] = 32436;
+      q = p;
+      p = 32436;
+
+    }
+  printf("%d", q);
+
+  return 0;
+}
